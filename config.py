0b1a8717d503c5023f9408ea6532ad142edc4e6b
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-06-07 01:42:14 +0530

    Update config.py
    Signed-off-by: Decent Boy <88884010+decentboyy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/0dfacd1c8fc60d6040a60.mp4")
+START_IMG = getenv("START_IMG", "https://graph.org/file/097a01650ced0e164e0ac.jpg")
 
 SESSION = getenv("SESSION", None)
 
